f5f751af31973da5f3353d455f65367f1a085dcf
Yuri <yuri.salgado.ft@gmail.com>
2024-08-22 11:50:16 -0300

    configurando a connection string pelo context, e adicionando o docker compose e as migrations

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -5,11 +5,17 @@ namespace TrilhaApiDesafio.Context
 {
     public class OrganizadorContext : DbContext
     {
+        public DbSet<Tarefa> Tarefas { get; set; }
         public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
         {
             
         }
 
-        public DbSet<Tarefa> Tarefas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost;Database=Organizador;User=SA;Password=Organizador1234!;TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 }
